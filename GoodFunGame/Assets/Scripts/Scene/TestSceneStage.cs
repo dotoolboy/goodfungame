9cd16ad51950593e1a6039fb22a5cc70d6399c61
Daniel Lee <jhoon8903@gmail.com>
2023-12-05 12:09:39 +0900

    [💫 Feature] Stage BackGround
    - Stage 중 BackGround 생성 작업
    - Addressable에 추가 완료
    - Scene Load 시에 "Background.prefab" 인스턴스화

---
+++ GoodFunGame/Assets/Scripts/Scene/TestSceneStage.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSceneStage : MonoBehaviour
+{
+    void Start()
+    {
+        if (Main.Resource.Loaded)
+        {
+            Main.Data.Initialize();
+        }
+        else
+        {
+            Main.Resource.LoadAllAsync<UnityEngine.Object>("PreLoad", (key, count, totalCount) =>
+            {
+                Debug.Log($"[TestScene] Load asset {key} ({count} / {totalCount})");
+                if (count >= totalCount)
+                {
+                    Main.Data.Initialize();
+                    Main.Game.Initialize();
+                    InitializeGame();
+                }
+            });
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < 5; i++)
+                Main.Object.Spawn<Enemy>("BOSS_MWJ", new Vector2(Random.Range(-5f, 5f), 5f));
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Main.Object.Despawn(FindObjectOfType<Enemy>());
+        }
+    }
+    private void InitializeGame()
+    {
+        // ====== 게임 시작 ======
+
+        GameObject newObj = Main.Resource.InstantiatePrefab("TempCharacter.prefab");
+        GameObject stageSprite = Main.Resource.InstantiatePrefab("BackGround.prefab");
+        Main.Object.Spawn<Player>("Player", Vector2.zero);
+        for (int i = 0; i < 5; i++)
+            Main.Object.Spawn<Enemy>("BOSS_MWJ", new Vector2(Random.Range(-5f, 5f), 5f));
+        // =======================
+    }
+
+}
--- GoodFunGame/Assets/UITestScene.cs
+++ GoodFunGame/Assets/UITestScene.cs
@@ -24,10 +24,9 @@ public class UITestScene : MonoBehaviour
 
     private void InitializeGame()
     {
-        Main.UI.ShowSceneUI<UI_Scene_Intro>();
+        // Main.UI.ShowSceneUI<UI_Scene_Intro>();
 
-
-       // Main.UI.ShowSceneUI<UI_Scene_Select>();
+        // Main.UI.ShowSceneUI<UI_Scene_Select>();
 
        // Main.UI.ShowSceneUI<UI_Scene_Battle>();
 
