01baba44521010556f9385ee6936ebd067215a5e
uhbbang3 <ansdnjswjd33@gmail.com>
2023-12-05 17:06:06 +0900

    [Feature] Enemy Move
    Enemy Move 구현
    Enemy Prefab 변경

--- GoodFunGame/Assets/Scripts/Models/Creature/Enemy.cs
+++ GoodFunGame/Assets/Scripts/Models/Creature/Enemy.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Enemy : Creature 
 {
@@ -29,7 +32,7 @@ public class Enemy : Creature
 
     private void Start()
     {
-
+        StartCoroutine(Move());
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -74,5 +77,105 @@ public class Enemy : Creature
         // TODO:: 오브젝트 디스폰
 
     }
+    #endregion
+
+    #region Move
+
+    [SerializeField] float moveMagnitude = 4f; // 움직임의 크기
+    [SerializeField] float moveFrequency = 3f; // 움직임의 빈도
+    [SerializeField] float moveTime = 2f;  // 움직이는 시간
+
+    IEnumerator Move()
+    {
+        while(State != CreatureState.DEAD)
+        {
+            System.Random random = new System.Random();
+            int randomNumber = random.Next(0, 3);
+
+            Vector3 originalPosition = transform.position;
+
+            switch (randomNumber)
+            {
+                case 0:
+                    yield return StartCoroutine(UpDown());
+                    break;
+
+                case 1:
+                    yield return StartCoroutine(LeftRight());
+                    break;
+
+                case 2:
+                    yield return StartCoroutine(Diagonal());
+                    break;
+                default:
+                    break;
+            }
+            yield return StartCoroutine(MoveToOriginalPosition(originalPosition, 0.15f));
+
+        }
+        yield break;
+    }
+
+    IEnumerator UpDown()
+    {
+        float elapsedTime = 0f;
+        Vector3 originalPosition = transform.position;
+
+        while (elapsedTime < moveTime)
+        {
+            float newY = originalPosition.y + Mathf.Sin(elapsedTime * moveFrequency) * moveMagnitude;
+            transform.position = new Vector3(originalPosition.x, newY, originalPosition.z);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator LeftRight()
+    {
+        float elapsedTime = 0f;
+        Vector3 originalPosition = transform.position;
+
+        while (elapsedTime < moveTime)
+        {
+            float newX = originalPosition.x + Mathf.Sin(elapsedTime * moveFrequency) * moveMagnitude;
+            transform.position = new Vector3(newX, originalPosition.y, originalPosition.z);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator Diagonal()
+    {
+        float elapsedTime = 0f;
+        Vector3 originalPosition = transform.position;
+
+        while (elapsedTime < moveTime)
+        {
+            float newX = originalPosition.x + Mathf.Sin(elapsedTime * moveFrequency) * moveMagnitude;
+            float newY = originalPosition.y + Mathf.Sin(elapsedTime * moveFrequency) * moveMagnitude;
+            transform.position = new Vector3(newX, newY, originalPosition.z);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        yield return StartCoroutine(MoveToOriginalPosition(originalPosition, 0.2f));
+    }
+
+    IEnumerator MoveToOriginalPosition(Vector3 originalPosition, float duration)
+    {
+        float elapsedTime = 0f;
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, originalPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = originalPosition;
+    }
+
     #endregion
 }
