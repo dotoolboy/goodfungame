188de6fab027ea5c285eef7beb01571731f3a056
uhbbang3 <ansdnjswjd33@gmail.com>
2023-12-07 04:57:55 +0900

    [Feature] HomingMissile Skill
    호밍미사일 스킬 구현

---
+++ GoodFunGame/Assets/Scripts/Models/Skills/Skill_HomigMissileObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Skill_HomigMissileObject : MonoBehaviour
+{
+    float _speed = 10f;
+
+    Rigidbody2D _rigidbody;
+    SpriteRenderer _sprite;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+
+        Vector2 dir = new Vector2(Random.Range(-1f, 1f), 0.5f).normalized;
+        _rigidbody.velocity = dir * _speed;
+
+        StartCoroutine(DestroyHomingMissile());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+            collision.gameObject.GetComponent<Enemy>().OnHit(Main.Object.Player);
+    }
+
+
+    IEnumerator DestroyHomingMissile()
+    {
+        yield return new WaitForSeconds(5f);
+        Main.Resource.Destroy(gameObject);
+    }
+}
