5263c0c9cae80aa6ee59c09e6a60a05caafb42cb
JacksonPepperoni <mashpota1014@gmail.com>
2023-12-07 09:38:33 +0900

    [🛠️ Bug Fix] Shop null fix2

--- GoodFunGame/Assets/Scripts/UI/Popup/UI_Popup_Status.cs
+++ GoodFunGame/Assets/Scripts/UI/Popup/UI_Popup_Status.cs
@@ -66,8 +66,8 @@ public class UI_Popup_Status : UI_Popup
 
         GetButton((int)Buttons.BackspaceBtn).gameObject.BindEvent(Close);
         SetSkill();
-     //   Main.Game.OnEquipChanged -= Refresh;
-     //   Main.Game.OnEquipChanged += Refresh;
+        Main.Game.OnEquipChanged -= Refresh;
+       Main.Game.OnEquipChanged += Refresh;
 
         Refresh();
 
@@ -82,15 +82,19 @@ public class UI_Popup_Status : UI_Popup
         GetText((int)Texts.GoldText).text = $"소지금 : {Main.Game.Gold}";
         GetText((int)Texts.NameText).text = $"{Main.Game.UserName}";
 
+
+        if (GetObject((int)GameObjects.skillSlotImg1).gameObject != null) { 
         GetObject((int)GameObjects.skillSlotImg1).gameObject.SetActive(false); // 장비변경 이벤트에 연결하면 장비창나갔다들어올때 null
         GetObject((int)GameObjects.skillSlotImg2).gameObject.SetActive(false);
         GetObject((int)GameObjects.skillSlotImg3).gameObject.SetActive(false);
-
+       
         for (int i = 0; i < Main.Game.EquippedSkills.Count; i++)
         {
             GetImage(i).sprite = Main.Resource.Load<Sprite>($"{Main.Data.Skills[Main.Game.EquippedSkills[i]].skillStringKey}.sprite");
             GetObject(i).gameObject.SetActive(true);
+            }
         }
+
 
     }
 
